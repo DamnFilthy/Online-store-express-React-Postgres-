e89bfd9916b2cc676439acc4a8a753f40452752c
Alex <alexander-shiryaev-1993@mail.ru>
2021-08-01 12:40:56 +0300

    auth middleware add

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (request, response, next) {
+    if (request.method === "OPTIONS") {
+        next()
+    }
+
+    try {
+        // Получение токена из headers отделив тип от самого токена
+        const token = request.headers.authorization.split(' ')[1]
+        if (!token) {
+            return response.status(401).json({ message: "Пользователь не авторизован" })
+        }
+
+        // Декодирвание токена 
+        const decoded = jwt.verify(token, process.env.SECRE_KEY)
+        request.user = decoded
+        next()
+
+    } catch (e) {
+        response.status(401).json({ message: "Пользователь не авторизован" })
+    }
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -14,10 +14,13 @@ const UserController = require('../controllers/userController')
 // Объект роутера 
 const router = new Router();
 
+// Импортируем middleware для авторизации
+const authMiddleware = require('../middleware/authMiddleware');
+
 // Методы роутера
 router.post('/registration', UserController.registration)
 router.post('/login', UserController.login)
-router.get('/auth', UserController.checkAuth)
+router.get('/auth', authMiddleware, UserController.checkAuth)
 
 // Экспортируем объект роутера 
 module.exports = router;
